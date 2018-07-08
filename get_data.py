33427c7c34a3df7718031dca477d8694cacf9c81
miskaka <miskaka0825@gmail.com>
2018-07-08 22:48:48 +0800

    get data from csv

--- get_data.py
+++ get_data.py
@@ -1,1 +1,10 @@
+# -*- coding:utf-8 -*-
+import csv
+import io
 
+data = []
+
+with open('3481.csv', mode='r', encoding='utf-8') as f: 
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
