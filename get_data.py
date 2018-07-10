2cc66c2a0f7192112cff5e8141ff69a500531232
miskaka <miskaka0825@gmail.com>
2018-07-10 11:52:13 +0800

    redo getdata

--- get_data.py
+++ get_data.py
@@ -4,24 +4,33 @@ import io
 import numpy
 import pandas as pd
 
-
+data = []
+data1 = [4,5,6]
+data2 = [7,8,9]
+data.append(data1)
+data.append(data2)
+print(data)
 
 with open('3481.csv', mode='r') as f:
+    '''
     date = []
     open = []
     close = []
     high = []
     low = []
     vol = []
+    '''
     reader = csv.reader(f)
     for row in reader:
-        print row
+        '''
         date.append(row[0])
         open.append(row[1])
         high.append(row[2])
         low.append(row[3])
         close.append(row[4])
         vol.append(row[6])
+        '''
+'''
 print open
 del open[0]
 del date[0]
@@ -34,3 +43,4 @@ open = [float(i) for i in high]
 open = [float(i) for i in low]
 open = [float(i) for i in close]
 print open
+'''
