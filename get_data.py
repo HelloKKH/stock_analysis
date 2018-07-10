f61eafc342e50289c3d8c89d72394b37ef1f1df8
miskaka <miskaka0825@gmail.com>
2018-07-10 10:17:38 +0800

    read strdata to floatdata

--- get_data.py
+++ get_data.py
@@ -1,10 +1,36 @@
 # -*- coding:utf-8 -*-
 import csv
 import io
+import numpy
+import pandas as pd
 
-data = []
 
-with open('3481.csv', mode='r') as f: 
+
+with open('3481.csv', mode='r') as f:
+    date = []
+    open = []
+    close = []
+    high = []
+    low = []
+    vol = []
     reader = csv.reader(f)
     for row in reader:
-        print(row)
+        print row
+        date.append(row[0])
+        open.append(row[1])
+        high.append(row[2])
+        low.append(row[3])
+        close.append(row[4])
+        vol.append(row[6])
+print open
+del open[0]
+del date[0]
+del high[0]
+del low[0]
+del close[0]
+del vol[0]
+open = [float(i) for i in open]
+open = [float(i) for i in high]
+open = [float(i) for i in low]
+open = [float(i) for i in close]
+print open
