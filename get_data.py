aab692d4d11d11f4021c807bf1016e6cebe0aba2
miskaka <miskaka0825@gmail.com>
2018-07-10 22:00:24 +0800

    finish be a function

--- get_data.py
+++ get_data.py
@@ -3,13 +3,12 @@ import csv
 import io
 import numpy
 import pandas as pd
+import test_draw
 
 data = []
-data1 = [4,5,6]
-data2 = [7,8,9]
-data.append(data1)
-data.append(data2)
-print(data)
+
+
+
 
 with open('3481.csv', mode='r') as f:
     '''
@@ -30,6 +29,14 @@ with open('3481.csv', mode='r') as f:
         close.append(row[4])
         vol.append(row[6])
         '''
+        if row[0] ==  '日期':
+            continue
+        data.append([row[0],float(row[1]),float(row[4]),float(row[2]),float(row[3]),int(row[6]),3481])
+del data[0]
+print data
+df2 = pd.DataFrame(data, columns=['date','open','close','high','low','volume','code'])
+print df2
+test_draw.draw_candle(df2)
 '''
 print open
 del open[0]
@@ -44,3 +51,4 @@ open = [float(i) for i in low]
 open = [float(i) for i in close]
 print open
 '''
+
